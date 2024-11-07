6641ebf6703dd9d3efbb03f4a0008a3a4e10ec8c
Anuj kumar <theanujsinha@gmail.com>
2024-11-07 14:23:24 +0530

    commit

---
+++ GRAPH/Graph Using adjacency matrix/AdjacencyMatrix.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void printGraph( vector<vector<int>>& adjacencyMatrix){
+    for(int i = 0; i < adjacencyMatrix.size(); i++){
+        cout << "Node " << i << " Neighbors: ";
+        for(int j = 0; j < adjacencyMatrix[i].size(); j++){
+            if(adjacencyMatrix[i][j] == 1){
+                cout << j << " ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+int main(){
+  
+    vector<vector<int>> edgeList = {
+        {0, 1}, {1, 2}, {2, 3}, {3, 0}
+    };
+    
+    int nodeCount = 4; // Number of nodes
+    vector<vector<int>> adjacencyMatrix(nodeCount, vector<int>(nodeCount, 0));
+
+    // Populate the adjacency matrix from edge list
+    for(int i = 0; i < edgeList.size(); i++){
+        int a = edgeList[i][0];  
+        int b = edgeList[i][1];  
+        adjacencyMatrix[a][b] = 1;
+        adjacencyMatrix[b][a] = 1;   
+    }
+
+    printGraph(adjacencyMatrix);
+
+    return 0;
+}

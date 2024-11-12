7a7a19dc132dcb07bd5b096721c7c6e8a6c5a6e6
Anuj kumar <theanujsinha@gmail.com>
2024-11-12 12:29:44 +0530

    commit

---
+++ RECURSION/Fibonaaci/fibonaaci.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int fibonaci(int n) {
+    if (n == 0 || n == 1)  // Base case
+        return 1;
+    
+    return fibonaci(n-2)+fibonaci(n - 1);  // Recursive call
+}
+
+int main() {
+    int number;
+    cout<<"Enter a num : ";
+    cin>>number;
+    cout << "Fibonaaci of " << number << " is " << fibonaci(number) << endl; 
+    return 0;
+}

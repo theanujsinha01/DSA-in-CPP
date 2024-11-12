e213c6a22c0b442537716bf9f9933b87fcbf6174
Anuj kumar <theanujsinha@gmail.com>
2024-11-12 13:31:52 +0530

    commit

---
+++ RECURSION/Count digit of num/countDigitNum.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int countDigits(int n) {
+    if (n == 0)  // Base case: if the number is 0, we have 0 digits
+        return 0;
+    return 1 + countDigits(n / 10);  // Recursive call: reduce the number by one digit
+}
+
+int main() {
+    int number;
+    cout << "Enter a number: ";
+    cin >> number;
+
+    // Special case: If the number is 0, the digit count is 1
+    if (number == 0)
+        cout << "Number of digits: 1" << endl;
+    else
+        cout << "Number of digits: " << countDigits(number) << endl;
+
+    return 0;
+}

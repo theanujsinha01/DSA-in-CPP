e26f03d557453b9e2845d41be35ad952b040e809
Anuj kumar <theanujsinha@gmail.com>
2025-01-22 18:46:11 +0530

    cmit

--- STRING/Count Palindromic string/CountPalindromicStr.cpp
+++ STRING/Count Palindromic string/CountPalindromicStr.cpp
@@ -1,35 +1,32 @@
-#include <string>
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int countPalindromesAroundCenter(const string& s, int left, int right) {
-    int count = 0;
+int longestPalindrome(string s) {
     int n = s.size();
+    int count = 0;
 
-    while (left >= 0 && right < n && s[left] == s[right]) {
-        count++;  // Count this palindrome
-        left--;
-        right++;
+    // For odd-length palindromes count
+    for (int i = 0; i < n; i++) {
+        int l = i, r = i;
+        while (l >= 0 && r < n && s[l] == s[r]) {
+            count++;
+            l--;
+            r++;
+        }
+    }
+
+    // For even-length palindromes count
+    for (int i = 0; i < n; i++) {
+        int l = i, r = i + 1;
+        while (l >= 0 && r < n && s[l] == s[r]) {
+            count++;
+            l--;
+            r++;
+        }
+       
     }
 
     return count;
-}
-
-int countSubstrings(string s) {
-    int n = s.size();
-    int totalCount = 0;
-
-    // For odd-length palindromes
-    for (int i = 0; i < n; i++) {
-        totalCount += countPalindromesAroundCenter(s, i, i);
-    }
-
-    // For even-length palindromes
-    for (int i = 0; i < n; i++) {
-        totalCount += countPalindromesAroundCenter(s, i, i + 1);
-    }
-
-    return totalCount;
 }
 
 int main() {
@@ -37,8 +34,9 @@ int main() {
     cout << "Enter a string: ";
     cin >> s;
 
-    int result = countSubstrings(s);
-    cout << "The total number of palindromic substrings is: " << result << endl;
+    int result = longestPalindrome(s);
+    cout << "The longest palindromic substring is: " << result << endl;
 
     return 0;
 }
+

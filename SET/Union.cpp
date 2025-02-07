b6f9e10d2e3177b6723bcd47dff35c60b907475a
Anuj kumar <theanujsinha@gmail.com>
2025-02-07 16:43:00 +0530

    commit

--- SET/Union.cpp
+++ SET/Union.cpp
@@ -6,7 +6,7 @@ int main() {
     set<int> s1 = {1, 2, 3};
     set<int> s2 = {3, 4, 5};
 
-    set<int> unionSet = s1;
+    set<int> unionSet(s1.begin(), s1.end());
     unionSet.insert(s2.begin(), s2.end());
 
     for (int x : unionSet) {

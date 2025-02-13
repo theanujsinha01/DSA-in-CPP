1b8d544c90240a8dc8007dc21b2fb5dfc124e9da
Anuj kumar <theanujsinha@gmail.com>
2025-02-13 15:23:02 +0530

    commit

--- SLIDING WINDOW/First Negative in Window/FirstNegativeInWindow.cpp
+++ SLIDING WINDOW/First Negative in Window/FirstNegativeInWindow.cpp
@@ -1,50 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> firstNegativeInWindow(int K, vector<int>& Arr, int N) {
-    deque<int> dq;  // Deque to store negative elements
-    vector<int> result;  // Stores first negative in each window
+vector<int> FirstNegativeInteger(vector<int>& arr, int k) {
+    int start = 0;
+    int end = start + k - 1;  
+    vector<int> ans;
 
-    // Process first K elements
-    for (int i = 0; i < K; i++) {
-        if (Arr[i] < 0) {
-            dq.push_back(Arr[i]);  // Store negative numbers
+    while (end < arr.size()) {  
+        bool found = false;
+        for (int i = start; i <= end; i++) {
+            if (arr[i] < 0) {
+                ans.push_back(arr[i]);
+                found = true;
+                break;
+            }
         }
+        if (!found) ans.push_back(0); 
+        
+        start++;
+        end++;
     }
-
-    // Process rest of the array
-    for (int i = K; i < N; i++) {
-        // Add the first negative element of the previous window
-        result.push_back(dq.empty() ? 0 : dq.front());
-
-        // Remove the element going out of the window
-        if (!dq.empty() && dq.front() == Arr[i - K]) {
-            dq.pop_front();
-        }
-
-        // Add new element if it's negative
-        if (Arr[i] < 0) {
-            dq.push_back(Arr[i]);
-        }
-    }
-
-    // Add the first negative of the last window
-    result.push_back(dq.empty() ? 0 : dq.front());
-
-    return result;
+    return ans;
 }
 
 int main() {
-    vector<int> arr = {1, -2, -3, 4, 5, -6, 7, 8};
-    int n = arr.size();
-    int k = 3;
+    vector<int> arr = {-8, 2, 3, -6, 10};
+    int k = 2;
     
-    vector<int> ans = firstNegativeInWindow(k, arr, n);
-
-    cout << "First negative integer in every window of size " << k << " is: ";
-    for (auto it : ans) {
-        cout << it << " ";
+    vector<int> result = FirstNegativeInteger(arr, k);
+    
+    for (int num : result) {
+        cout << num << " ";
     }
-
+    
     return 0;
 }

86e5c2a49b5acdfae21576f66d0b570d31f0759c
Anuj kumar <theanujsinha@gmail.com>
2025-02-16 15:19:31 +0530

    commit

--- BASICS/Fibonacci Series/FibonacciSeries.cpp
+++ BASICS/Fibonacci Series/FibonacciSeries.cpp
@@ -1,21 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void printFibonacci(int n) {
-    int a = 0, b = 1, nextTerm;
-
-    // Print the first two terms
-    cout << "Fibonacci Series: " << a << " " << b << " ";
-
-    // Loop to calculate and print the rest of the terms
-    for (int i = 3; i <= n; ++i) {
-        nextTerm = a + b;
-        cout << nextTerm << " ";
-        a = b;
-        b = nextTerm;
-    }
-
-    cout << endl;
+int printFibonacci(int n) {
+   if (n <= 1) {
+       return n;
+   }
+    return printFibonacci(n - 1) + printFibonacci(n - 2);
 }
 
 int main() {
@@ -23,12 +13,7 @@ int main() {
     cout << "Enter the number of terms: ";
     cin >> n;
 
-    // Print the Fibonacci series
-    if (n >= 2) {
-        printFibonacci(n);
-    } else {
-        cout << "The number of terms must be greater than or equal to 2." << endl;
-    }
-
+    cout << "Fibonacci Series: ";
+    cout << printFibonacci(n) << endl;
     return 0;
 }

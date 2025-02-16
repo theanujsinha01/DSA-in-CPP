9ef20cddd91a142c31342e0afdf31500bdf03298
Anuj kumar <theanujsinha@gmail.com>
2025-02-16 16:28:16 +0530

    commit

--- ARRAY/Largest subarray with 0 sum/LargestSubarray0Sum.cpp
+++ ARRAY/Largest subarray with 0 sum/LargestSubarray0Sum.cpp
@@ -3,12 +3,9 @@ using namespace std;
 
 int maxLenSubarrayWithZeroSum(const vector<int>& arr) {
     int maxLen = 0;
-
-    // Outer loop for the starting index of the subarray
     for (int i = 0; i < arr.size(); ++i) {
         int sum = 0; // Initialize sum for the current subarray
 
-        // Inner loop for the ending index of the subarray
         for (int j = i; j < arr.size(); ++j) {
             sum = sum + arr[j]; // Add the current element to the sum
 
--- ARRAY/Longest Consecutive Sequence/LongestConsecutiveSequence.cpp
+++ ARRAY/Longest Consecutive Sequence/LongestConsecutiveSequence.cpp
@@ -1,27 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
 int findLongestConsecutiveSequence(int arr[], int n) {
-    if (n == 0) return 0;
+    
 
     sort(arr, arr + n);
     int longestStreak = 1, currentStreak = 1;
-
+    
     for (int i = 1; i < n; i++) {
         if (arr[i] == arr[i - 1] + 1) {
             currentStreak++;
-        } else if (arr[i] != arr[i - 1]) {
-            longestStreak = max(longestStreak, currentStreak);
+        } 
+        else{
             currentStreak = 1;
         }
+        longestStreak = max(longestStreak, currentStreak); 
     }
 
     return max(longestStreak, currentStreak);  // In case the longest streak ends at the last element
 }
 
 int main() {
-    int arr[] = {100, 4, 2, 1, 3, 10};
+    int arr[] = {11, 12, 2, 1, 3, 10};
     int n = sizeof(arr) / sizeof(arr[0]);
     cout << "Longest Consecutive Sequence Length: " << findLongestConsecutiveSequence(arr, n) << endl;
     return 0;

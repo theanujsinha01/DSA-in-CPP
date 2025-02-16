42e291e3562c89b8459a00f53f83d2562909209c
Anuj kumar <theanujsinha@gmail.com>
2025-02-16 15:56:27 +0530

    commit

--- BASICS/Pascal Triangle/PascalTriangle.cpp
+++ BASICS/Pascal Triangle/PascalTriangle.cpp
@@ -1,26 +1,24 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
     int n;
-
-    // Input the number of rows
     cout << "Enter number of rows: ";
     cin >> n;
 
     // Pascal's Triangle logic
-    for (int i = 0; i < n; i++) {
+    for (int i = 1; i <= n; i++) {
         int value = 1; // The first value in every row is 1
 
         // Print spaces for alignment
-        for (int j = 0; j < n - i - 1; j++) {
+        for (int j = 1; j <= n - i; j++) {
             cout << " ";
         }
-
+        
         // Print values in the row
-        for (int j = 0; j <= i; j++) {
-            cout << value << " ";
-            value = value * (i - j) / (j + 1); // Update value for the next column
+        for (int j = 1; j <= i; j++) {
+            cout << value <<" ";
+            value = value * (i - j) / j; // Update value for the next column
         }
         cout << endl;
     }

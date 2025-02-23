2280ffbdc72ac3f9f2aca364419383306406f3cb
Anuj kumar <theanujsinha@gmail.com>
2025-02-23 13:59:40 +0530

    commit

---
+++ BINARY SEARCH TREE/Deletion Node From BST/Deletion_From_BST.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node *left, *right;
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+// Find the inorder successor (smallest in right subtree)
+Node* minValueNode(Node* node) {
+    while (node->left != NULL)
+        node = node->left;
+    return node;
+}
+
+// Delete node from BST
+Node* deleteNode(Node* root, int key) {
+    if (root == NULL)
+        return root;
+
+    if (key < root->data)
+        root->left = deleteNode(root->left, key);
+    else if (key > root->data)
+        root->right = deleteNode(root->right, key);
+    else {
+        // Node with one or no child
+        if (root->left == NULL) {
+            Node* temp = root->right;
+            delete root;
+            return temp;
+        } else if (root->right == NULL) {
+            Node* temp = root->left;
+            delete root;
+            return temp;
+        }
+
+        // Node with two children
+        Node* temp = minValueNode(root->right);
+        root->data = temp->data;
+        root->right = deleteNode(root->right, temp->data);
+    }
+    return root;
+}
+
+// Inorder Traversal
+void inorder(Node* root) {
+    if (root != NULL) {
+        inorder(root->left);
+        cout << root->data << " ";
+        inorder(root->right);
+    }
+}
+
+// Main Function
+int main() {
+    Node* root = new Node(50);
+    root->left = new Node(30);
+    root->right = new Node(70);
+    root->left->left = new Node(20);
+    root->left->right = new Node(40);
+    root->right->left = new Node(60);
+    root->right->right = new Node(80);
+
+    cout << "Inorder before deletion: ";
+    inorder(root);
+    cout << endl;
+
+    root = deleteNode(root, 20); // Deleting node with value 20
+
+    cout << "Inorder after deletion: ";
+    inorder(root);
+    cout << endl;
+
+    return 0;
+}

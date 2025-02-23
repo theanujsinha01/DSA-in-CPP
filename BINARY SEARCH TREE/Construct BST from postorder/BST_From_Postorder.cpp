27262acd0dc2f587ceffed1bd033e3c56aad8889
Anuj kumar <theanujsinha@gmail.com>
2025-02-23 13:17:26 +0530

    commit

---
+++ BINARY SEARCH TREE/Construct BST from postorder/BST_From_Postorder.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct Node
+{
+    int data;
+    Node *left;
+    Node *right;
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+      
+};
+Node* Insert(Node* &root, int key) {
+      if (root == NULL) {
+          root = new Node(key);
+          return root;
+      }
+      if (root->data < key) {
+          Insert(root->right, key);
+      } else {
+          Insert(root->left, key);
+      }
+      return root;
+}
+  
+Node* BST_From_Postorder(vector<int>& postorder) {
+    Node* root = NULL;  // Initialize root to NULL
+    for (int i = postorder.size() - 1; i>=0; i--) {
+        Insert(root, postorder[i]);  // Insert the actual value, not the index
+    }
+    return root;
+}
+  
+void InOrderTraversal(Node *root) {
+    if (root == NULL) return;
+    InOrderTraversal(root->left);
+    cout << root->data << " ";
+    InOrderTraversal(root->right);
+}
+  
+int main() {
+    vector<int>postorder = {5,2,1,3,6};
+    Node* result = BST_From_Postorder(postorder);
+    InOrderTraversal(result);
+    return 0;
+}

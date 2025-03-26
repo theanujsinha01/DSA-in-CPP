246b600af69bae5a8e7c5fb628ad2c4e03980b1b
Anuj kumar <theanujsinha@gmail.com>
2025-03-26 15:01:04 +0530

    commit

--- ARRAY/Alternative Positive Negative/AlternativePositiveNegative.cpp
+++ ARRAY/Alternative Positive Negative/AlternativePositiveNegative.cpp
@@ -3,10 +3,9 @@ using namespace std;
 
 void rearrangeArray(vector<int>& nums) {
     int n = nums.size();
-    vector<int> po; // To store positive numbers
-    vector<int> ne; // To store negative numbers
+    vector<int> po; 
+    vector<int> ne; 
 
-    // Separate positive and negative numbers
     for (int i = 0; i < n; i++) {
         if (nums[i] >= 0) {
             po.push_back(nums[i]);
@@ -15,7 +14,6 @@ void rearrangeArray(vector<int>& nums) {
         }
     }
 
-    // Rearrange the array with alternating positive and negative numbers
     for (int i = 0, j = 0, k = 0; i < n; i++) {
         if (i % 2 == 0) {
             nums[i] = po[j++];
@@ -27,12 +25,10 @@ void rearrangeArray(vector<int>& nums) {
     
 }
 
-// Example usage
 int main() {
     vector<int> nums = {3, 11, 2, -2, -5, -3};
     rearrangeArray(nums);
 
-    // Print rearranged array
     for(int i = 0; i < nums.size(); i++) {
         cout << nums[i] << " ";
     }

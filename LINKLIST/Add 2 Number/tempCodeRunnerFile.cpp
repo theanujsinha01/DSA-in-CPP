2ee789064a2df492ae0ddf55f430b3c2b8b1a79e
Anuj kumar <theanujsinha@gmail.com>
2025-05-12 22:45:43 +0530

    commit

--- LINKLIST/Add 2 Number/tempCodeRunnerFile.cpp
+++
@@ -1,2 +0,0 @@
-
-    result = rev(result);
--- LINKLIST/Remove Loop/tempCodeRunnerFile.cpp
+++
@@ -1,5 +0,0 @@
-  // // Move both pointers until they meet at the loop start
-    // while (ptr2 != ptr1) {
-    //     ptr1 = ptr1->next;
-    //     ptr2 = ptr2->next;
-    // }

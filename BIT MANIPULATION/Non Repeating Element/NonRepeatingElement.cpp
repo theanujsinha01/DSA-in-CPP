2712b6e9de6974566da453ad21140157b0f84861
Anuj kumar <theanujsinha@gmail.com>
2025-05-13 14:18:49 +0530

    commit

--- BIT MANIPULATION/Non Repeating Element/NonRepeatingElement.cpp
+++ BIT MANIPULATION/Non Repeating Element/NonRepeatingElement.cpp
@@ -1,8 +1,9 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 // Function to find the only non-repeating element in the array
-int findNonRepeatingElement(int arr[], int n) {
+int findNonRepeatingElement(vector<int>& arr) {
+    int n = arr.size();
     int result = 0;
     for (int i = 0; i < n; i++) {
         result = result ^ arr[i];  // XOR all the elements
@@ -11,13 +12,9 @@ int findNonRepeatingElement(int arr[], int n) {
 }
 
 int main() {
-    int arr[] = {2, 3, 5, 4, 5, 3, 4};  // Example array
-    int n = sizeof(arr) / sizeof(arr[0]);
-
-    int result = findNonRepeatingElement(arr, n);
-    
+    vector<int>v = {2, 3, 5, 4, 5, 3, 4};  // Example array
+    int result = findNonRepeatingElement(v);
     cout << "The only non-repeating element is: " << result << endl;
-
     return 0;
 }
 //time complexity: O(n)
